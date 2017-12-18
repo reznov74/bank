153989ed299f37a1686ba1c2edf4b4f8ff8d5dc0
reznov47 <razavi_rohani@yahoo.com>
2017-12-18 10:58:45 -0800

    start impl design

--- src/main/java/software/eng/project/bank/core/controller/CustomerController.java
+++ src/main/java/software/eng/project/bank/core/controller/CustomerController.java
@@ -1,9 +1,24 @@
 package software.eng.project.bank.core.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import software.eng.project.bank.core.model.Account.Account;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/customer")
 public class CustomerController {
+
+    @RequestMapping(value = "/accounts",
+            method = RequestMethod.GET,
+            produces = {"application/json", "application/xml"})
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    Account getAccounts(HttpServletResponse response)
+    {
+        response.setStatus(200);
+        //get user information from jwt
+        return null;
+    }
 }
--- src/main/java/software/eng/project/bank/core/model/Account/Account.java
+++ src/main/java/software/eng/project/bank/core/model/Account/Account.java
@@ -1,4 +1,20 @@
 package software.eng.project.bank.core.model.Account;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "account")
 public class Account {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "USER_ID", length = 100)
+    @NotNull
+    @Size(min = 1, max = 100)
+    private long accountNumber;
+    
 }
